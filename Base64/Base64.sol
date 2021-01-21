2f0cf2b9077131eb18b26a87315c99dac9a38fa5
mikhail <mikhail.ushakov@yahoo.com>
2021-01-21 12:26:34 +0300

    Base64 interface

---
+++ Base64/Base64.sol
@@ -0,0 +1,25 @@
+pragma solidity >= 0.6.0;
+
+interface IBase64 {
+
+    function encode(uint32 answerId, string str) external returns (string base64);
+    function decode(uint32 answerId, string base64) external returns (string str);
+
+}
+
+library Base64 {
+
+	uint256 constant DEBOT_ADDR = 0x8913b27b45267aad3ee08437e64029ac38fb59274f19adca0b23c4f957c8cfa1;
+	int8 constant DEBOT_WC = -31;
+
+	function encode(uint32 answerId, string str) public pure {
+		address addr = address.makeAddrStd(DEBOT_WC, DEBOT_ADDR);
+		IBase64(addr).encode(answerId, str);
+	}
+
+	function decode(uint32 answerId, string base64) public pure {
+		address addr = address.makeAddrStd(DEBOT_WC, DEBOT_ADDR);
+		IBase64(addr).decode(answerId, base64);
+	}	
+
+} 
---
+++ Base64/examples/Example.sol
@@ -0,0 +1,24 @@
+pragma solidity >= 0.6.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+import "Base64.sol";
+
+
+contract ExampleContract {
+
+    function test() public {
+        string str = "aaa";
+	Base64.encode(tvm.functionId(setEncode), str);
+	string base64 = "YWFh";
+        Base64.decode(tvm.functionId(setDecode), base64);
+    }
+
+    function setEncode(string base64) public pure {
+        require(base64=="YWFh",100);
+    }
+
+    function setDecode(string str) public pure {
+        require(str=="aaa",100);
+    }
+} 
