bab3dfb5a441830ccb12c078411dbb4491fb7cc3
NikitaM <monakhovnik@gmail.com>
2021-02-11 15:33:52 +0300

    Update Terminal/Terminal.sol
    Co-authored-by: Roman Tishakov <romantishakov@gmail.com>

--- Terminal/Terminal.sol
+++ Terminal/Terminal.sol
@@ -17,25 +17,9 @@ library Terminal {
 	uint256 constant ID = 0x8796536366ee21852db56dccb60bc564598b618c865fc50c8b1ab740bba128e3;
 	int8 constant DEBOT_WC = -31;
 
-	function inputStr(uint32 answerId, string prompt, bool multiline) public pure {
+	function input(uint32 answerId, string prompt, bool multiline) public pure {
 		address addr = address.makeAddrStd(DEBOT_WC, ID);
 		ITerminal(addr).inputStr(answerId, prompt, multiline);
-	}
-	function inputInt(uint32 answerId, string prompt) public pure {
-		address addr = address.makeAddrStd(DEBOT_WC, ID);
-		ITerminal(addr).inputInt(answerId, prompt);
-	}
-	function inputUint(uint32 answerId, string prompt) public pure {
-		address addr = address.makeAddrStd(DEBOT_WC, ID);
-		ITerminal(addr).inputUint(answerId, prompt);
-	}
-	function inputTons(uint32 answerId, string prompt) public pure {
-		address addr = address.makeAddrStd(DEBOT_WC, ID);
-		ITerminal(addr).inputTons(answerId, prompt);
-	}
-	function inputBoolean(uint32 answerId, string prompt) public pure {
-		address addr = address.makeAddrStd(DEBOT_WC, ID);
-		ITerminal(addr).inputBoolean(answerId, prompt);
 	}
 	function print(uint32 answerId, string message) public pure {
 		address addr = address.makeAddrStd(DEBOT_WC, ID);
