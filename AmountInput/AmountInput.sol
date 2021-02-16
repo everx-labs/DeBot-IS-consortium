6d7e695b488c5647f370482eb7138c26fff73105
Roman Tishakov <romantishakov@gmail.com>
2021-02-16 18:01:13 +0300

    Update AmountInput/AmountInput.sol

--- AmountInput/AmountInput.sol
+++ AmountInput/AmountInput.sol
@@ -11,7 +11,7 @@ library AmountInput {
 	uint256 constant ID = 0xa1d347099e29c1624c8890619daf207bde18e92df5220a54bcc6d858309ece84;
 	int8 constant DEBOT_WC = -31;
 
-	function get(uint32 answerId, string prompt, uint8 decimals, uint256 min, uint256 max) public pure {
+	function get(uint32 answerId, string prompt, uint8 decimals, uint128 min, uint128 max) public pure {
 		address addr = address.makeAddrStd(DEBOT_WC, ID);
 		IAmountInput(addr).get(answerId, prompt, decimals, min, max);
 	}
