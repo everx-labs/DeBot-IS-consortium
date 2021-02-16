7ae001fefd5f7083a1a32a71bf82b107c9285d39
Roman Tishakov <romantishakov@gmail.com>
2021-02-16 18:00:53 +0300

    Update AmountInput/AmountInput.hpp

--- AmountInput/AmountInput.hpp
+++ AmountInput/AmountInput.hpp
@@ -2,7 +2,7 @@ namespace tvm { namespace schema {
 
 __interface IAmountInput {
 	[[internal, answer_id]]
-	uint256 get(string prompt, uint8 decimals, uint256 min, uint256 max);
+	uint128 get(string prompt, uint8 decimals, uint128 min, uint128 max);
 }
 
 }};
