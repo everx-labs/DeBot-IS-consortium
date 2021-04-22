9fab04c93e7112a9a0dad4526f8d0ccce5a7daef
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2021-04-22 14:12:51 +0300

    update

--- Hex/Hex.sol
+++ Hex/Hex.sol
@@ -9,16 +9,16 @@ interface IHex {
 
 library Hex {
 
-	uint256 constant ITF_ADDR = 0xedfbb00d6ebd16d57a1636774845af9499b400ba417da8552f40b1250256ff8f;
+	uint256 constant ID = 0xedfbb00d6ebd16d57a1636774845af9499b400ba417da8552f40b1250256ff8f;
 	int8 constant DEBOT_WC = -31;
 
 	function encode(uint32 answerId, bytes data) public pure {
-		address addr = address.makeAddrStd(DEBOT_WC, ITF_ADDR);
+		address addr = address.makeAddrStd(DEBOT_WC, ID);
 		IHex(addr).encode(answerId, data);
 	}
 
 	function decode(uint32 answerId, string hexstr) public pure {
-		address addr = address.makeAddrStd(DEBOT_WC, ITF_ADDR);
+		address addr = address.makeAddrStd(DEBOT_WC, ID);
 		IHex(addr).decode(answerId, hexstr);
 	}	
 
