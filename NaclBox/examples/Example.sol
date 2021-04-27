b69584e568c19b1e28f249f823dddb050aefee75
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2021-04-27 10:03:40 +0300

    example fix

--- NaclBox/examples/Example.sol
+++ NaclBox/examples/Example.sol
@@ -1,30 +1,3 @@
-pragma solidity >= 0.6.0;
-pragma AbiHeader expire;
-pragma AbiHeader time;
-pragma AbiHeader pubkey;
-import "SigningBoxInput.sol";
-import "NaclBox.sol";
-
-
-contract ExampleContract {
-
-	uint256 m_signingBox;
-	uint256 m_curve25519PublicKey;
-
-	function start() public {
-		SigningBoxInput.get(tvm.functionId(setSigningBox),"Select signing box",[]);
-	}
-
-	function setSigningBox(uint256 publicKey) public {
-		m_signingBox = publicKey;
-		NaclBox.getPublicKey(tvm.functionId(setCurve256PublicKey),m_signingBox);
-	}
-	function setCurve256PublicKey(uint256 encryptPublicKey) public {
-		m_curve25519PublicKey = publicKey;
-	}
-    
-}
-
 pragma ton-solidity >=0.40.0;
 pragma AbiHeader expire;
 pragma AbiHeader time;
