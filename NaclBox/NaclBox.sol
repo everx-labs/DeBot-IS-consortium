ede0f9d97a3b558a0c434347a9e9954dcd3e20a0
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2021-04-27 09:41:40 +0300

    rename to ID

--- NaclBox/NaclBox.sol
+++ NaclBox/NaclBox.sol
@@ -7,19 +7,19 @@ function decrypt(uint32 answerId, bytes encrypted, bytes nonce, uint256 theirEnc
 
 library NaclBox {
 
-	uint256 constant DEBOT_ADDR = da6e573c0282785d9f8e88a81e0f2665811148cb3bd9b5dcce6a079ab7009958;
+	uint256 constant ID = da6e573c0282785d9f8e88a81e0f2665811148cb3bd9b5dcce6a079ab7009958;
 	int8 constant DEBOT_WC = -31;
 
 	function getPublicKey(uint32 answerId, uint32 edHandle) public pure {
-		address a = address.makeAddrStd(DEBOT_WC, DEBOT_ADDR);
+		address a = address.makeAddrStd(DEBOT_WC, ID);
 		INaclBox(a).getPublicKey(answerId, edHandle);
 	}
 	function encrypt(uint32 answerId, bytes decrypted, bytes nonce, uint256 theirEncryptPublicKey, uint32 edHandle) public pure {
-		address a = address.makeAddrStd(DEBOT_WC, DEBOT_ADDR);
+		address a = address.makeAddrStd(DEBOT_WC, ID);
 		INaclBox(a).encrypt(answerId, decrypted, nonce, their_public, edHandle);
 	}
 	function decrypt(uint32 answerId, bytes encrypted, bytes nonce, uint256 theirEncryptPublicKey, uint32 edHandle) public pure {
-		address a = address.makeAddrStd(DEBOT_WC, DEBOT_ADDR);
+		address a = address.makeAddrStd(DEBOT_WC, ID);
 		INaclBox(a).decrypt(answerId, encrypted, nonce, theirEncryptPublicKey, edHandle);
 	}
 }
