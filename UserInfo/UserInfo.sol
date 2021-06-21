c02d839a252e583e55375705dedb0db19da64bdb
Nikita.m <nikita.m@tonlabs.io>
2021-06-21 16:11:23 +0300

    Add getSigningBox func

--- UserInfo/UserInfo.sol
+++ UserInfo/UserInfo.sol
@@ -4,6 +4,7 @@ interface IUserInfo {
 
     function getAccount(uint32 answerId) external returns (address value);
     function getPublicKey(uint32 answerId) external returns (uint256 value);
+    function getSigningBox(uint32 answerId) external returns (uint32 value);
 
 }
 
@@ -22,12 +23,17 @@ library UserInfo {
         IUserInfo(addr).getPublicKey(answerId);
     }
 
+    function getSigningBox(uint32 answerId) public pure {
+        address addr = address.makeAddrStd(DEBOT_WC, ID);
+        IUserInfo(addr).getSigningBox(answerId);
+    }
+
 }
 
 contract UserInfoABI is IUserInfo {
 
     function getAccount(uint32 answerId) external override returns (address value) {}
     function getPublicKey(uint32 answerId) external override returns (uint256 value) {}
-
+    function getSigningBox(uint32 answerId) external override returns (uint32 value) {}
 }
 
