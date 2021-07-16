58fcaf553f46287608dcb324eb6d1a839b6cff5c
Nikita.m <nikita.m@tonlabs.io>
2021-07-16 19:33:03 +0300

    Update example and abi

--- DateTimeInput/examples/Example.sol
+++ DateTimeInput/examples/Example.sol
@@ -4,29 +4,41 @@ pragma AbiHeader time;
 pragma AbiHeader pubkey;
 import "https://raw.githubusercontent.com/tonlabs/debots/main/Debot.sol";
 import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Terminal/Terminal.sol";
-import "../DateInput.sol";
+import "../DateTimeInput.sol";
 
 contract ExampleContract is Debot {
 
+    int128 m_datestamp;
     function start() public override {
-        DateInput.get(tvm.functionId(setDate), 
+        DateTimeInput.getDate(tvm.functionId(setDate), 
             "Choose a day in 2021 from the begining until current day:", 
-            Date(1, 1, 2021, 0), Date(0, 0, 0, int128(now)));
+            1609448400, int128(now));
     }
 
-    function setDate(Date date) public {
+    function setDate(int128 date) public {
         // TODO: continue here
-        Terminal.print(0, format("{}/{}/{}, unixtime: {}", date.day, date.month, date.year, date.unixtime));
+        Terminal.print(0, format("Date unixtime: {}", date));
+        m_datestamp = date;
+        DateTimeInput.getTime(tvm.functionId(setTime), 
+            "Choose a day time (local):", 
+            55800, 86100, 1, 0x7FFF);
+    }
+
+    function setTime(uint32 time, int16 timeZoneOffset) public {
+        // TODO: continue here
+        Terminal.print(0, format("Time timestamp: {}, time zone: {}", time, timeZoneOffset));
+
+        Terminal.print(0, format("Date + time : {}", time + m_datestamp));
     }
 
     function getDebotInfo() public functionID(0xDEB) override view returns(
         string name, string version, string publisher, string caption, string author,
         address support, string hello, string language, string dabi, bytes icon
     ) {
-        name = "DateInput example DeBot";
+        name = "DateTimeInput example DeBot";
         version = "0.1.0";
         publisher = "TON Labs";
-        caption = "How to use the DateInput interface";
+        caption = "How to use the DateTimeInput interface";
         author = "TON Labs";
         support = address(0);
         hello = "Hello, i am an example DeBot.";
@@ -36,7 +48,7 @@ contract ExampleContract is Debot {
     }
 
     function getRequiredInterfaces() public view override returns (uint256[] interfaces) {
-        return [ Terminal.ID, DateInput.ID ];
+        return [ Terminal.ID, DateTimeInput.ID ];
     }
 
 }
