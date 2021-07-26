11b8edaf9df80099c81ce5cefb5c71988df21e6a
Nikita.m <nikita.m@tonlabs.io>
2021-07-26 11:58:59 +0300

    Add files for Media iface

---
+++ Media/README.md
@@ -0,0 +1,65 @@
+# Media Interface
+
+**Status**: Proposed
+
+| Name         | ID                                                                |
+| :--------    | :---------------------------------------------------------------- |
+| Media        | 59cdc2aafe53760937dac5b1c4b89ce12950f56a56298108a987cfe49b7c84b5  |
+
+
+## Description
+
+Allows to show media of different formats (images, docs and others) to the user.
+
+`output` - shows media to the user.
+
+arguments:
+
+	answerId: uint32 - function id of result callback
+
+    prompt: bytes - utf-8 string to print to the user on top of media.
+
+    data: bytes - utf-8 string with encoded media according to RFC2397.
+    
+returns:
+
+	result: MediaStatus - status of media output.
+
+`getSupportedMediatypes` - returns array of supported mediatypes.
+
+arguments:
+
+	answerId: uint32 - function id of result callback
+    
+returns:
+
+	mediatypes: bytes[] - array of strings with supported mediatypes. Each string is in the format of "type/subtype". E.g. ["image/png", "image/jpg"].
+
+## Declaration in Solidity
+
+```jsx
+enum MediaStatus {
+    Success,
+    UnsupportedMediatype,
+    InvalidDataScheme
+}
+
+interface IMedia {
+    function output(uint32 answerId, string prompt, string data) external returns (MediaStatus result);
+    function getSupportedMediatypes(uint32 answerId) external returns (string mediatypes);
+}
+```
+
+## Declaration in C++
+
+TODO: add later
+
+## Code Example
+
+### Solidity
+
+examples/Example.sol
+
+### C++
+
+TODO: add later
---
+++ Media/examples/Example.sol
@@ -0,0 +1,48 @@
+pragma ton-solidity >=0.45.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+import "https://raw.githubusercontent.com/tonlabs/debots/main/Debot.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Terminal/Terminal.sol";
+import "../Media.sol";
+
+contract ExampleContract is Debot {
+
+    string m_pngBase64;
+    function start() public override {
+        Media.getSupportedMediatypes(tvm.functionId(setMediatypes));
+    }
+
+    function setMediatypes(string[] mediatypes) public {
+        for (string mtype: mediatypes) {
+            Terminal.print(0, mtype);
+        }
+        Media.output(tvm.functionId(setResult), "PNG image", format("data:image/png;base64,{}", m_pngBase64));
+    }
+
+    function setResult(MediaStatus result) public pure {
+        require(result == MediaStatus.Success);
+        // TODO: continue here.
+    }
+
+    function getDebotInfo() public functionID(0xDEB) override view returns(
+        string name, string version, string publisher, string caption, string author,
+        address support, string hello, string language, string dabi, bytes icon
+    ) {
+        name = "Media example DeBot";
+        version = "0.1.0";
+        publisher = "TON Labs";
+        caption = "How to use Media interface";
+        author = "TON Labs";
+        support = address(0);
+        hello = "Hello, i am an example DeBot.";
+        language = "en";
+        dabi = m_debotAbi.get();
+        icon = "";
+    }
+
+    function getRequiredInterfaces() public view override returns (uint256[] interfaces) {
+        return [ Terminal.ID, Media.ID ];
+    }
+
+}
