86f0f04afd6bb8118682c63b30e813e856889d39
Nikita.m <nikita.m@tonlabs.io>
2021-09-07 13:02:34 +0300

    Add todos

--- Query/TonMessageLib.sol
+++ Query/TonMessageLib.sol
@@ -4,21 +4,25 @@ library TonMessageLib {
     
 	function src(TvmCell message) public returns (address) {
         TvmSlice slice = message.toSlice();
+        // TODO: decode source address.
         return slice.decode(address);
     }
 
     function dst(TvmCell message) public returns (address) {
         TvmSlice slice = message.toSlice();
+        // TODO: decode dest address.
         return slice.decode(address);
     }
 
     function balance(TvmCell message) public returns (uint64) {
         TvmSlice slice = message.toSlice();
+        // TODO: decode balance.
         return slice.decode(uint64);
     }
 
     function body(TvmCell message) public returns (TvmCell) {
         TvmSlice slice = message.toSlice();
+        // TODO: decode message body.
         return slice.decode(TvmCell);
     }
 }
