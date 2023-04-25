c20e60f7133f689b692254527358f710e06c5ace
Nikita.m <nikita.m@tonlabs.io>
2023-04-25 14:45:22 +0300

    Add SttingLib.stou

--- libraries/StringLib.sol
+++ libraries/StringLib.sol
@@ -14,4 +14,16 @@ library StringLib {
         if(!addr.hasValue()) return null;
         return address.makeAddrStd(int8(wc.get()), uint(addr.get()));
     }
+
+    function stou(string str, byte t) public returns (optional(uint256)) {
+        optional(int) num;
+        if (t == 'x') {
+            num = stoi(format("0x{}",str));
+        }
+        if (t == 'd') {
+            num = stoi(str);
+        }
+        if (!num.hasValue()) return null;
+        return uint(num.get());
+    }
 }
